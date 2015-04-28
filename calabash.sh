36b45f8235e766b0aafea3323942389a1e9ab9e8
Gerrit Imsieke <gerrit.imsieke@le-tex.de>
2015-04-28 10:34:54 +0200

    schemas

--- calabash.sh
+++ calabash.sh
@@ -62,7 +62,7 @@ if [ -z $UI_LANG ]; then
 fi
 
 if [ -z $CFG ]; then
-    CFG=$EXT_BASE/ltx/ltx-config.xml
+    CFG=$EXT_BASE/transpect/transpect-config.xml
 fi
 
 # SAXON_JAR is the path to a Saxon PE or EE jar file. Its name should match the following
@@ -72,7 +72,7 @@ if [ ! -z $SAXON_JAR ]; then
     SAXON_PROCESSOR=--saxon-processor=${SAXON_JAR:(-6):2}
 fi
 
-CLASSPATH="$SAXON_JAR:$EXT_BASE/ltx/ltx-validate-with-rng/jing.jar:$DISTRO/xmlcalabash-1.1.0-96.jar:$DISTRO/lib/:$DISTRO/lib/xmlresolver.jar:$DISTRO/lib/htmlparser-1.4.jar:$PROJECT_DIR/adaptions/common/calabash:$DISTRO/lib/org.restlet.jar:$EXT_BASE/ltx/ltx-validate-with-rng/:$EXT_BASE/ltx/ltx-unzip/:$EXT_BASE/ltx/ltx-image-identify:$EXT_BASE/ltx/ltx-image-identify/commons-imaging-1.0-SNAPSHOT.jar:$EXT_BASE/ltx/ltx-image-identify/xmlgraphics-commons-1.5.jar:$EXT_BASE/fop/fop.jar:$DISTRO/lib/tagsoup-1.2.1.jar:$CLASSPATH"
+CLASSPATH="$SAXON_JAR:$EXT_BASE/transpect/rng-extension/jing.jar:$DISTRO/xmlcalabash-1.1.0-96.jar:$DISTRO/lib/:$DISTRO/lib/xmlresolver.jar:$DISTRO/lib/htmlparser-1.4.jar:$PROJECT_DIR/adaptions/common/calabash:$DISTRO/lib/org.restlet.jar:$EXT_BASE/transpect/rng-extension/:$EXT_BASE/transpect/unzip-extension/:$EXT_BASE/transpect/image-props-extension:$EXT_BASE/transpect/image-props-extension/commons-imaging-1.0-SNAPSHOT.jar:$EXT_BASE/transpect/image-props-extension/xmlgraphics-commons-1.5.jar:$DISTRO/lib/tagsoup-1.2.1.jar:$CLASSPATH"
 
 OSDIR=$DIR
 if $cygwin; then
