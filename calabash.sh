70462bf8120cba616d15be8b7e3614879fa175af
Gerrit Imsieke <gerrit.imsieke@le-tex.de>
2019-01-09 16:09:05 +0100

    Make file.encoding overwriteable
    Apparently, CP1252-encoded HTML pages that set the encoding via meta element and that are parsed by validator.nu will be read according to file.encoding. If it is set to utf-8, the encoding will be garbled.

--- calabash.sh
+++ calabash.sh
@@ -63,6 +63,10 @@ if [ -z $HEAP ]; then
     HEAP=1024m
 fi
 
+if [ -z $JAVA_FILE_ENCODING ]; then
+    JAVA_FILE_ENCODING=utf-8
+fi
+
 if [ -z $UI_LANG ]; then
     UI_LANG=en
 fi
@@ -139,7 +143,7 @@ fi
 
 $JAVA \
    -cp "$CLASSPATH" \
-   -Dfile.encoding=UTF-8 \
+   -Dfile.encoding=$JAVA_FILE_ENCODING \
    "-Dxml.catalog.files=$CATALOGS" \
    -Djruby.compile.mode=OFF \
    -Dxml.catalog.staticCatalog=1 \
