86e81a5b9af958973c9501392be9b8ad3b14548e
Gerrit Imsieke <gerrit.imsieke@le-tex.de>
2021-12-12 11:58:58 +0100

    log4j

--- calabash.bat
+++ calabash.bat
@@ -1,1 +1,1 @@
-java -Xmx1024m -cp C:\cygwin64\home\paul\calabash-100/xmlcalabash-1.3.2-100.jar com.xmlcalabash.drivers.Main %*
+java -Xmx1024m -cp C:\cygwin64\home\paul\calabash-100/xmlcalabash-1.3.2-100.jar -Dlog4j2.formatMsgNoLookups=true com.xmlcalabash.drivers.Main %*
